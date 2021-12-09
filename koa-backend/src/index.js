234d9df82838b478dee90eb5bb0611dd8caa6439
chris.lee <neoxrock@gmail.com>
2021-12-09 11:26:05 +1100

    init server

--- koa-backend/src/index.js
+++ koa-backend/src/index.js
@@ -1,1 +1,10 @@
-const hi = 'hi';
+const Koa = require('koa');
+const app = new Koa();
+
+app.use((ctx) => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(4000, () => {
+  console.log('server is running at http://localhost:4000');
+});

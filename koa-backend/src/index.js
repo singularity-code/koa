80060a7f4cf61f11e46729422e8682d40ff17cf5
chris.lee <neoxrock@gmail.com>
2021-12-09 11:54:20 +1100

    test middleware

--- koa-backend/src/index.js
+++ koa-backend/src/index.js
@@ -1,8 +1,29 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use((ctx) => {
+app.use(async (ctx, next) => {
   ctx.body = 'Hello World';
+  console.log(ctx.url);
+  console.log(1);
+  if (ctx.query.authorized !== '1') {
+    ctx.status = 401;
+    ctx.throw(401, 'Unauthorized');
+  }
+  await next();
+  console.log('END')
+  // without await
+  //   next().then(() => {
+  //     console.log('END');
+  //   });
+});
+
+app.use((ctx, next) => {
+  console.log(2);
+  next();
+});
+
+app.use((ctx, next) => {
+  console.log('hi');
 });
 
 app.listen(4000, () => {

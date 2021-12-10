4f9cb382120d6ad3d34349f48db9ae64d1d69d80
chris.lee <neoxrock@gmail.com>
2021-12-10 11:50:09 +1100

    params and  query

--- koa-backend/src/index.js
+++ koa-backend/src/index.js
@@ -13,6 +13,17 @@ router.get('/about', async (ctx) => {
   ctx.body = 'About';
 });
 
+router.get('/about/:name?', async (ctx) => {
+  const { name } = ctx.params;
+  ctx.body = name ? `Name ${name}` : 'Name';
+});
+
+router.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+  ctx.body = id ? `Post ${id}` : 'Post does not exist';
+});
+
+// apply router middleware
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {

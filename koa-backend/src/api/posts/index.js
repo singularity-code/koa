6b20640110e4a7b7194b05bb616dd5c28d067f96
chris.lee <neoxrock@gmail.com>
2021-12-13 16:10:47 +1100

    koa rounting controller

--- koa-backend/src/api/posts/index.js
+++ koa-backend/src/api/posts/index.js
@@ -1,18 +1,11 @@
 const Router = require('koa-router');
+const postsController = require('./posts.ctrl');
 const posts = new Router();
 
-const printInfo = (ctx) => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
-
-posts.get('/', printInfo);
-posts.post('/', printInfo);
-posts.get('/:id', printInfo);
-posts.delete('/:id', printInfo);
-posts.put('/:id', printInfo);
-posts.patch('/:id', printInfo);
+posts.get('/', postsController.list);
+posts.post('/', postsController.write);
+posts.get('/:id', postsController.read);
+posts.delete('/:id', postsController.remove);
+posts.put('/:id', postsController.replace);
+posts.patch('/:id', postsController.update);
 module.exports = posts;
